3429fd92572c4cf0e1d9e42555ccbadd699351f0
michabay05 <111288777+michabay05@users.noreply.github.com>
2022-12-09 17:24:49 -0500

    Updated the bitboard printing

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -7,7 +7,7 @@ void printBits(const uint64_t bitboard) {
     for (int r = 0; r < 8; r++) {
         std::cout << "  " << 8 - r << " |";
         for (int f = 0; f < 8; f++)
-            std::cout << " " << getBit(bitboard, r * 8 + f);
+            std::cout << " " << (getBit(bitboard, r * 8 + f) ? "1" : ".");
         std::cout << "\n";
     }
     std::cout << "      - - - - - - - -\n      a b c d e f g h\n";
